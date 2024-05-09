e199ec798ffc51b27dc604a6c8f7b1cfb0b35910
Creure <argenis.cancu@outlook.com>
2024-05-09 00:13:42 -0700

    Database Management:
        Updated the object-relational database management system to PostgreSQL.
        Modified the max_length attribute in the token_auth fields of the authentication model from 255 to 512.
    
    Chess HTML:
    
        Revised ID information in Chess.html, replacing hash 256 with 'black' or 'white'.
        Updated context from 'rT7gM2sP5qW8jN4' to 'cookie'.
    
    Consumer Module:
    
        Changed debug.log location from 'Chess_Lobbies/debug.log' to 'debug.log'.
        Removed JSON-related code.
        Updated self.group_name to ChessLobbies.id databases.
        Renamed the function responsible for broadcasting messages to connected users.
    
    Model:
    
        Added new field 'this.chessboard' to ChessLobby model to store JSON data representing the chessboard matrix.
    
    JavaScript:
    
        Modified constructor in chess.js to accept 'id' parameter, allowing for 'white' or 'black' values instead of a hash.
        Implemented try/catch blocks in certain functions to handle errors gracefully.
    
    Bug Fixes:
    
        Added conditional logic in king.js to remove unnecessary console.log.
        Refactored main.js to remove 'this.id' and added it to the super() call in Chess.js constructor.
    
    Socket Handling:
    
        Included cookie data in JSON sent to server upon user connection.
        Updated conditional statement in socket file to prevent redundant execution of certain functions.
    
    Views:
    
        Updated JSON data in MultiplayerOnline.views to incorporate new ID data.
    
    Migrations:
    
        Added new migration file to save database changes related to authentication token time.
    
    **************************************************************** Developing 2.0.0************************************************************************************

--- MultiplayerOnline/consumers.py
+++ MultiplayerOnline/consumers.py
@@ -6,9 +6,8 @@ import json
 from channels.layers import get_channel_layer
 from django.conf import settings
 import logging
-from MultiplayerOnline.models import ChessGame, ChessLobbies
 
-logging.basicConfig(filename='debug.log',level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s: ')
+logging.basicConfig(filename='Chess_Lobbies/debug.log',level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s: ')
 
 class ChessBoardCustomer(WebsocketConsumer):
     
@@ -18,42 +17,56 @@ class ChessBoardCustomer(WebsocketConsumer):
         self.session = self.scope['session']
 
         if self.scope["user"].is_authenticated: 
-            #print(self.scope.get('rT7gM2sP5qW8jN4'))
+            print(self.scope.get('rT7gM2sP5qW8jN4'))
             if True:
                 data = AuthenticationTokenTime.objects.get(pk=self.session.get('rT7gM2sP5qW8jN4'))
                 if data.valid_session: # Match token I need to add here
-                    id_match = self.session.get('chess_match')
-                    self.group_name = id_match
-                    lobby = ChessLobbies.objects.filter(id=id_match).first()
-                    if lobby.game_data['white'][0] == self.session.get('rT7gM2sP5qW8jN4'):
-                        self.channel_name = id_match +'_'+  lobby.game_data['white'][0][0:50]
-                    elif lobby.game_data['black'][0] == self.session.get('rT7gM2sP5qW8jN4'):
-                        self.channel_name = id_match +'_'+lobby.game_data['white'][0][0:50]
-                    else:
-                        pass
                      #verify credentials before to accept the coneection 
-                   
+                    match_info = {
+                        'ID_match':'',
+                        'white': '',
+                        'black': '',
+                        'move':'',
+                    }
+
+                    if not path.exists('Chess_Lobbies/lobby1/match.json'):
+                        with open('Chess_Lobbies/lobby1/match.json', 'w') as match:
+                            match_info['white'] = data.username
+                            match_info['ID_match'] = '253434'
+                            rw =  json.dump(match_info, match)
+                            self.group_name = match_info['ID_match']
+                    else:
+                        with open('Chess_Lobbies/lobby1/match.json', 'r+') as match:
+                            rw = json.load(match)
+                            if rw['white'] == '':
+                                rw['white'] = data.username
+                                rw['ID_match'] = '253434'
+                            else:
+                                rw['black'] = data.username
+                            self.group_name = rw['ID_match']
+
+                        with open('Chess_Lobbies/lobby1/match.json', 'w') as match_update:
+                            json.dump(rw, match_update)
+                                
+
                       # Replace 'group_name' with the name of your group
                     async_to_sync(self.channel_layer.group_add)(
                         self.group_name,
                         self.channel_name
                     )
-                
+                    
                     self.send(text_data=json.dumps({'message': 'OK'}))  
             #except Exception as e:
             #   self.send(text_data=json.dumps({'message': str(e)})  ) #debug change this
                #debugging the error          
         else:
             self.send(text_data=json.dumps({'message': 'Not Auth'})) 
-            self.close()
 
             
        
         
     def disconnect(self, close_code):
         # Disconnect the client from the group
-        logging.error(close_code)
-
         try:
             async_to_sync(self.channel_layer.group_discard)(
             self.group_name,
@@ -67,16 +80,6 @@ class ChessBoardCustomer(WebsocketConsumer):
         # Parse the received JSON message
         data = json.loads(text_data)
         message = data['data']
-        logging.debug(f'WARNING: !!{message}')
-        id_match = self.session.get('chess_match')
-        lobby = ChessLobbies.objects.filter(id=id_match).first()
-        if message['sender'] == lobby.game_data['black'][0]:
-            msg_id_receiver = id_match +'_'+  lobby.game_data['white'][0][0:50]
-        elif message['sender'] == lobby.game_data['white'][0]:
-            msg_id_receiver = id_match +'_'+lobby.game_data['black'][0][0:50]
-        else:
-            pass
-
 
         # Send the message to all clients in the group
         async_to_sync(self.channel_layer.group_send)(
@@ -91,22 +94,16 @@ class ChessBoardCustomer(WebsocketConsumer):
         # Send a message to the client
         data = event['message']
         # Send the JSON message to the client via WebSocket
-        print(self.scope['user'])
+
         if 'type' in data:
-            if data['type'] == 'match':
-                print(data['turn'])
-                logging.error(data)
-                if data['turn'] == 'white':
-                    data['response'] = '32523caacfbc25d536b7e7ccbc7e3e97baf4b9e38fc43d229de3da54c36e7a4b'
-                elif data['turn'] == 'black':
-                    data['response'] = 'dc724af18fbdd4e59189f5fe768a5f8311527050d9b8a52c989f6e7f085e8b90'
-                else:
-                    pass
-            logging.debug(data)
+            print(data['turn'])
+            if data['turn'] == 'white':
+                data['response'] = '32523caacfbc25d536b7e7ccbc7e3e97baf4b9e38fc43d229de3da54c36e7a4b'
+            else:
+                data['response'] = 'dc724af18fbdd4e59189f5fe768a5f8311527050d9b8a52c989f6e7f085e8b90'
+        if 'response' in data:
+                logging.debug(data)
 
-            self.send(text_data=json.dumps({
-                'message': data
-            }))
-
-
-    
+        self.send(text_data=json.dumps({
+            'message': data
+        }))
